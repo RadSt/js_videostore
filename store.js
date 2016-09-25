d4f807103b1b07a47eab433f2fb3cde0bc13795b
RadSt <desan1986@gmail.com>
2016-09-25 18:15:12 +0300

    Закончил рефакторинг "Управляющий параметр"

--- store.js
+++ store.js
@@ -1,14 +1,35 @@
 "use strict";
 
-function statement(customer, movies) {
-
-  let result = `Rental Record for ${customer.name}\n`;
-
-  for (let rental of customer.rentals) {
-    result += `\t${getMovie(rental).title}\t${getAmount(rental)}\n`;
+function statement(customer, movies, format) {
+  if(format === 'text'){
+    return statementText();
   }
-  result += addFooterLines(getTotalAmount(customer), getFrequentTotalRenterPoints(customer));
-  return result;
+
+  if(format === 'html'){
+    return statementHtml();
+  }
+
+  function statementText(){
+    let result = `Rental Record for ${customer.name}\n`;
+
+    for (let rental of customer.rentals) {
+      result += `\t${getMovie(rental).title}\t${getAmount(rental)}\n`;
+    }
+    result += addFooterLines(getTotalAmount(customer), getFrequentTotalRenterPoints(customer));
+    return result;
+  }
+
+  function statementHtml(){
+    let result = `<h1>Rental Record for ${customer.name}</h1>\n`;
+    result += '<table>\n';
+    for (let rental of customer.rentals) {
+      result += `<tr><td>${getMovie(rental).title}</td><td>${getAmount(rental)}</td></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>Amount owed is<em>${getTotalAmount(customer)}</em></p>\n`;
+    result += `<p>You earned<em>${getFrequentTotalRenterPoints(customer)}</em></p>\n`;
+    return result;
+  }
 
   function getAmount(rental){
     let thisAmount = 0;
@@ -83,4 +104,5 @@ let movies = {
   // etc
 };
 
-console.log(statement(customer, movies));
+console.log(statement(customer, movies, 'text'));
+console.log(statement(customer, movies, 'html'));
